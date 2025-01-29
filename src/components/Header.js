9da23ee06fabacf2f03ed722127f7d8de6a9cedc
Sharonesaa <oriannaesaa07@gmail.com>
2025-01-28 23:31:12 -0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,32 @@
 import React from "react";
+import "../styles/header.css"; // Asegúrate de importar el archivo CSS
 
 function Header() {
   return (
-    <header>
-      <h1>Mi Portafolio</h1>
-      <nav>
-        <a href="#about">Sobre mí</a>
-        <a href="#projects">Proyectos</a>
-        <a href="#contact">Contacto</a>
-      </nav>
+    <header className="header">
+      <div className="container">
+        {/* Logo */}
+        <h1 className="logo">Mi Portafolio</h1>
+        
+        {/* Enlaces de navegación */}
+        <nav>
+          <ul className="nav-links">
+            <li><a href="#about">Sobre mí</a></li>
+            <li><a href="#projects">Proyectos</a></li>
+            <li><a href="#contact">Contacto</a></li>
+          </ul>
+        </nav>
+        
+        {/* Iconos de redes sociales */}
+        <div className="social-icons">
+          <i className="fab fa-facebook"></i>
+          <i className="fab fa-twitter"></i>
+          <i className="fab fa-instagram"></i>
+        </div>
+      </div>
     </header>
   );
 }
 
 export default Header;
+
